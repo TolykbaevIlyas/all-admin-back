6712596a344377fd6a64e5d43e29a4424e44d97e
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-04-28 00:22:46 +0500

    upd cors

--- api/login.ts
+++ api/login.ts
@@ -42,9 +42,15 @@ const sessions = new Map();
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
 
 
   if (req.method === 'POST') {
