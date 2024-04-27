8f4167665dea0837b084f70cb6ece903fff4ffdd
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-04-27 22:30:41 +0500

    ReINit

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import {sql} from "@vercel/postgres"
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
