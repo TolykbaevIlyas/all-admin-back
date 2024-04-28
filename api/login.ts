f8deb8bd62fe71b80cbefeb56f959526ebc39d48
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-04-29 01:22:25 +0500

    Ups login

--- api/login.ts
+++ api/login.ts
@@ -1,39 +1,3 @@
-
-/**
- * @swagger
- * tags:
- *   name: Authentication
- *   description: Endpoints for user authentication
- */
-
-/**
- * @swagger
- * /api/auth/login:
- *   post:
- *     summary: Authenticate user
- *     tags: [Authentication]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               username:
- *                 type: string
- *               password:
- *                 type: string
- *             required:
- *               - username
- *               - password
- *     responses:
- *       '200':
- *         description: Authentication successful
- *       '401':
- *         description: Invalid username or password
- *       '405':
- *         description: Method not allowed
- */
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { sql } from '@vercel/postgres';
 import { sha3_512 } from 'js-sha3';
@@ -55,8 +19,8 @@ const allowCors = fn => async (req, res) => {
   return await fn(req, res)
 }
 
-// const sessions = new Map();
-async function handler(req, res) {
+const sessions = new Map();
+async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'POST') {
     const { username, password } = req.body;
     try {
@@ -64,20 +28,20 @@ async function handler(req, res) {
       const userQuery = await sql`SELECT * FROM users WHERE username = ${username} AND password_hash = ${hashedPassword}`;
 
       if (userQuery.rows.length === 1) {
-        // const sessionId = generateSessionId();
-        // sessions.set(sessionId, username);
-        //res.setHeader('Set-Cookie', `sessionId=${sessionId}; HttpOnly; Secure; SameSite=Strict`);
+        const sessionId = generateSessionId();
+        sessions.set(sessionId, username);
+        res.setHeader('Set-Cookie', `sessionId=${sessionId}; HttpOnly; Secure; SameSite=Strict`);
 
-        return res.status(200).json({ message: 'Авторизация успешна' });
+        res.status(200).json({ message: 'Авторизация успешна' });
       } else {
-        return res.status(401).json({ error: 'Неверные имя пользователя или пароль' });
+        res.status(401).json({ error: 'Неверные имя пользователя или пароль' });
       }
     } catch (error) {
       console.error('Ошибка при попытке авторизации:', error);
-      return res.status(500).json({ error: 'Что-то пошло не так' });
+      res.status(500).json({ error: 'Что-то пошло не так' });
     }
   } else {
-    return res.status(405).json({ error: 'Метод не разрешен' });
+    res.status(405).json({ error: 'Метод не разрешен' });
   }
 }
 
