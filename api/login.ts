eb93d80f3210dea67a667631c80d1d04b216ab23
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-04-28 16:36:58 +0500

    Upd vercel.json

--- api/login.ts
+++ api/login.ts
@@ -42,19 +42,19 @@ const sessions = new Map();
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.status(200).end();
+    // res.setHeader('Access-Control-Allow-Credentials', 'true')
+    // res.setHeader('Access-Control-Allow-Origin', '*')
+    // // another common pattern
+    // // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    // res.setHeader(
+    //   'Access-Control-Allow-Headers',
+    //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    // )
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'POST');
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // res.status(200).end();
 
 
   if (req.method === 'POST') {
