de01de1e0d42ffe65ce411f8aeeeb1d9202a3639
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-04-28 18:42:14 +0500

    upd cors login

--- api/login.ts
+++ api/login.ts
@@ -41,22 +41,6 @@ import { sha3_512 } from 'js-sha3';
 const sessions = new Map();
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    
-    // res.setHeader('Access-Control-Allow-Credentials', 'true')
-    // res.setHeader('Access-Control-Allow-Origin', '*')
-    // // another common pattern
-    // // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    // res.setHeader(
-    //   'Access-Control-Allow-Headers',
-    //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    // )
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader('Access-Control-Allow-Methods', 'POST');
-    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    // res.status(200).end();
-
-
   if (req.method === 'POST') {
     const { username, password } = req.body;
 
@@ -67,6 +51,13 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       if (userQuery.rows.length === 1) {
         const sessionId = generateSessionId();
         sessions.set(sessionId, username);
+        res.setHeader('Access-Control-Allow-Credentials', 'true')
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      )
         res.setHeader('Set-Cookie', `sessionId=${sessionId}; HttpOnly; Secure; SameSite=Strict`);
 
         res.status(200).json({ message: 'Авторизация успешна' });
