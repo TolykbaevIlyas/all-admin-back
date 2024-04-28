20bdea880cfbfe5cb44e9b84e5361f604dda4116
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-04-28 18:49:58 +0500

    upd cors

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  allowCors({req,res})
   return res.json({
     message: `Hello go to /api/docs!`,
   })
